a4f19eb5eb919ca3986f45a4093ad12112de3966
forhadmia1 <forhad8613@gmail.com>
2026-08-19 01:47:45 +0600

    initialize project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -62,6 +63,9 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button >
+            title
+          </Button>
         </div>
       </main>
     </div>
